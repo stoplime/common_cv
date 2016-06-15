c0b471a811e311c54e5d721cbd7b330d0db07616
Steffen Lim <slanimero@gmail.com>
2016-06-14 20:19:33 -0400

    update from last test

--- src/rviz_pos_test.cpp
+++ src/rviz_pos_test.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include "nav_msgs/Odometry.h"
+#include <visualization_msgs/Marker.h>
 
 using namespace std;
 
@@ -35,10 +36,10 @@ void keyboard_callback(const keyboard::Key& msg)
 			pos.x-=speed;
 			break;
 		case 97:
-			pos.y-=speed;
+			pos.y+=speed;
 			break;
 		case 100:
-			pos.y+=speed;
+			pos.y-=speed;
 			break;
 		case 32:
 			pos.z+=speed;
@@ -49,7 +50,49 @@ void keyboard_callback(const keyboard::Key& msg)
 		//default:
 			
 	}
+}
+
+visualization_msgs::Marker create_marker()
+{
+	visualization_msgs::Marker marker;
 	
+	// Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "/my_frame";
+    marker.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = "basic_shapes";
+    marker.id = 0;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    marker.type = visualization_msgs::Marker::CUBE;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = visualization_msgs::Marker::ADD;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = 0;
+    marker.pose.position.y = 0;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = 1.0;
+    marker.scale.y = 1.0;
+    marker.scale.z = 1.0;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 1.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    //marker.lifetime = ros::Duration();
+    return marker;
 }
 
 int main(int argc, char **argv)
@@ -58,7 +101,7 @@ int main(int argc, char **argv)
 	
 	ros::NodeHandle n;
 	
-	ros::Publisher odroid_pub = n.advertise<nav_msgs::Odometry>("pose", 1);
+	ros::Publisher odroid_pub = n.advertise<visualization_msgs::Marker>("pose", 1);
 	ros::Subscriber keyboard_input = n.subscribe("/keyboard/keydown", 1, keyboard_callback);
 	
 	ros::Rate looprate(10);
@@ -69,9 +112,10 @@ int main(int argc, char **argv)
 		std_msgs::String msg;
 		string s = "Pos: "+patch::to_string(pos.x)+", "+patch::to_string(pos.y)+", "+patch::to_string(pos.z);
 		
-		nav_msgs::Odometry pose;
-		pose.pose.pose.position = pos;
-		odroid_pub.publish(pose);
+		visualization_msgs::Marker marker;
+		marker = create_marker();
+		marker.pose.position = pos;
+		odroid_pub.publish(marker);
 		cout << s << endl;
 		
 		ros::spinOnce();
